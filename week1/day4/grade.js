7798032ab0952d6b7261a46bc6b199a26c4c101b
Narahari M V <97274974+naraharimv2@users.noreply.github.com>
2026-02-19 17:39:48 +0530

    Add files via upload

---
+++ week1/day4/grade.js
@@ -0,0 +1,17 @@
+let marks = 85;
+
+if (marks >= 90) {
+    console.log("Grade A");
+} 
+else if (marks >= 75) {
+    console.log("Grade B");
+} 
+else if (marks >= 50) {
+    console.log("Grade C");
+} 
+else if (marks >= 40) {
+    console.log("Pass");
+} 
+else {
+    console.log("Fail");
+}
